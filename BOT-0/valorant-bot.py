5a134025fac820c5000d0fa99ea2659a8ced2e96
TomasChacur95 <tomasdatasc@gmail.com>
2021-11-30 17:06:08 -0300

    BotValorantV1.2

--- BOT-0/valorant-bot.py
+++ BOT-0/valorant-bot.py
@@ -2,7 +2,6 @@ import discord
 import webbrowser
 from discord.ext import commands
 
-
 bot = commands.Bot(command_prefix = "!")
 inicio_sesion = 0
 
@@ -13,13 +12,14 @@ async def on_ready():
     inicio_sesion = 1
 
 @bot.command()
-async def agente (ctx, arg1):         
-    if(arg1 != "viper" and arg1 != "omen" and arg1 != "jett" and arg1 != "astra" and arg1 != "breach" and arg1 != "brimstone" and arg1 != "chamber" 
-    and arg1 != "cypher" and arg1 != "kayo" and arg1 != "killjoy" and arg1 != "phoenix" and arg1 != "raze" and arg1 != "reyna" and arg1 != "sage" and arg1 != "sky" 
-    and arg1 != "sova" and arg1 != "yoru"):
-        pido_agente = await ctx.reply("El Agente solicitado no existe, por favor ingresa un Agente valido.")
-    else:
-        pido_agente = await ctx.reply("Aquí tienes algunos lineups y trucos con "+ arg1.capitalize() + ("  https://blitz.gg/valorant/guides?agent=" + arg1.lower() + "&difficulty=all&map=all&side=both&ability=all"))
+async def agente(ctx, arg1):
+    personajes = ["Astra", "Breach", "Brimstone", "Chamber", "Cypher", "Jett", "Kayo", "Killjoy", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye", "Sova", "Viper", "Yoru"]
+    input_user = arg1.capitalize()
+    if(input_user not in personajes):
+        pido_agente = await ctx.reply("El Agente "+ str(input_user) + " no existe, por favor ingresa un Agente valido. " + "Aquí tienes una lista de todos los Agentes de Valorant: \n" + '\n'.join(personajes))
         
-   
+    else:
+        pido_agente = await ctx.reply("Aquí tienes algunos lineups y trucos con " + input_user + ("  https://blitz.gg/valorant/guides?agent=" + input_user.lower() + "&difficulty=all&map=all&side=both&ability=all"))
+        
+
 bot.run('Token')
