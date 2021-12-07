4eb2deadc4e0a137c3cbfd9dc9ccf1ac24a24c51
nfernandezsanz <nfernndezsanz@frba.utn.edu.ar>
2021-12-07 10:47:23 -0300

    06122021

--- BOT-0/role-bot.py
+++ BOT-0/role-bot.py
@@ -5,8 +5,10 @@ from discord.ext import commands
 from discord.utils import get
 
 
-bot = commands.Bot(command_prefix = "!")
+bot   = commands.Bot(command_prefix = "!")
 login = 0
+
+IDMensaje = 0
 
 @bot.event
 async def on_ready():
@@ -15,21 +17,49 @@ async def on_ready():
     login = 1
 
 
+
+@bot.event
+async def on_raw_reaction_add(ctx): 
+    global IDMensaje
+    if(IDMensaje == 0):
+        print("No configuraste el BOT todavia...")
+        return 0
+    emoji                = ctx.emoji
+    
+    if(ctx.message_id == IDMensaje and emoji == ":eye:"):
+        print("Reaccion")
+
+
+@bot.event
+async def on_raw_reaction_add(ctx):
+    lista_emojis = ["🦸‍♀️","⚔","🧙‍♀️","🔫"]
+    alias_emojis = ["Valorant","WOW","LOL","CS"]
+    global IDMensaje
+
+    if(IDMensaje == ctx.message_id):
+        #print("Reaccionaron al msg...")
+        member = ctx.member
+        guild  = ctx.guild
+        emoji  = ctx.emoji.name
+        if(emoji in lista_emojis):
+            #print("Asigno rol de Valorant a " + str(member))
+            nombre_rol = alias_emojis[lista_emojis.find(emoji)]
+            role = discord.utils.get(guild.roles, name = nombre_rol)
+            await member.add_roles(role)
+
+
 @bot.command()
-async def roles(ctx):
-    reaction = await ctx.reply("Select your Game" + '\n' + '\n' + "- Valorant 🦸‍♀️" '\n' + "- World of Warcraft ⚔" + '\n' + "- League of Legends 🧙‍♀️" + '\n' + "- Cs:Go 🔫")
-    valorant   = await reaction.add_reaction('🦸‍♀️')
-    wow        = await reaction.add_reaction('⚔')
-    league     = await reaction.add_reaction('🧙‍♀️')
-    csgo       = await reaction.add_reaction('🔫')
+async def setupRoles(ctx):
+    global IDMensaje
+    reaction   = await ctx.reply("Select your Game" + '\n' + '\n' + "- Valorant 🦸‍♀️" '\n' + "- World of Warcraft ⚔" + '\n' + "- League of Legends 🧙‍♀️" + '\n' + "- Cs:Go 🔫")
+    IDMensaje  = reaction.message_id
+
+    await reaction.add_reaction('🦸‍♀️')
+    await reaction.add_reaction('⚔')
+    await reaction.add_reaction('🧙‍♀️')
+    await reaction.add_reaction('🔫')
 
     
-
-    
-    
-    
-
-
 bot.run("Your Code")
     
     
