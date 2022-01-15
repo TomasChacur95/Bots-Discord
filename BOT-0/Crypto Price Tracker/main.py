6bbca2afaefa13747dd921d3e3e35be903b9f872
Tom Chacur <tomasdatasc@gmail.com>
2022-01-15 13:31:34 -0300

    CryptoPriceTrackerChanges15/01/2022

--- BOT-0/Crypto Price Tracker/main.py
+++ BOT-0/Crypto Price Tracker/main.py
@@ -5,6 +5,7 @@ import random
 from discord        import activity
 from discord.ext    import tasks, commands
 import secrets      as scrt
+import blockchain   as blq
 from discord.utils  import get
 from aiohttp.client import request
 from discord.ext    import commands
@@ -14,16 +15,9 @@ from discord.ext.commands.core import check, command
 
 # bot = scrt.bot
 bot   = commands.Bot(command_prefix = "!")
+
 login = 0
 
-
-
-tokens_dict = {
-    'morw' :     '0x6b61b24504a6378e1a99d2aa2a5efcb1f5627a3a',
-    'slp'  :     '0xcc8fa225d80b9c7d42f96e9570156c65d6caaa25',
-    'pvu'  :     '0x31471e0791fcdbe82fbf4c44943255e923f1b794',
-    'eternal' :  '0xd44fd09d74cd13838f137b590497595d6b3feea4'
-}
 
 # Login
 @bot.event
@@ -33,15 +27,26 @@ async def on_ready():
     login = 1
     my_background_task.start()
 
+
+
+# Bot Activity with Crypto Prices
+@tasks.loop(seconds=120.0)
+async def my_background_task():
+    await bot.change_presence(activity=discord.Game(name='XLS : '))
+    await bot.change_presence(activity=discord.Game(name='Eternal $ '))
+
+
+
+blq.tokens_dict
+
+
+
 # Coin Price Command
 @bot.command()
 async def coin(ctx, arg1):
-    global tokens_dict
-    if tokens_dict == tokens_dict:
-        async with aiohttp.ClientSession() as cs:
-            async with cs.get('https://api.pancakeswap.info/api/v2/tokens/' + tokens_dict[arg1]) as r:
-                res = await r.json()  # returns dict
-                await ctx.reply(res['data']['price'])
+    
+    if blq.tokens_dict == blq.tokens_dict:
+        await blq.blockchain_connection (ctx, arg1)
     else:
         await ctx.reply("The token " + str(arg1) +  " is not in the token list, if you want to add " + str(arg1) + " to the list please use the command : " + '\n' + "!add_token and insert key and value with an space on each")
         
@@ -49,24 +54,12 @@ async def coin(ctx, arg1):
 
 @bot.command()
 async def add_token(ctx, key, value):
-    global tokens_dict
 
-    if(key in tokens_dict.keys()):
+    if(key in blq.tokens_dict.keys()):
         await ctx.reply('I already have this token on my list, please add another Contract or another Value')
     else:
-        tokens_dict[key] = value
+        blq.tokens_dict[key] = value
 
-
-
-@tasks.loop(seconds=120.0)
-async def my_background_task():
-    """Will loop every 60 seconds and change the bots presence"""
-    await bot.change_presence(activity=discord.Game(name='XLS : '))
-    await bot.change_presence(activity=discord.Game(name='BNB : '))
-
-
-
-    
-
+ 
 bot.run(scrt.bot_token)
 
