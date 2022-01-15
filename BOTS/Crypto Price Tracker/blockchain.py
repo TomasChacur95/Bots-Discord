6a513ce935b35865fdcf485be13e6eb464c415d3
Tom Chacur <tomasdatasc@gmail.com>
2022-01-15 13:34:28 -0300

    FolderChange

---
+++ BOTS/Crypto Price Tracker/blockchain.py
@@ -0,0 +1,22 @@
+import discord 
+import json
+import aiohttp
+from discord.utils  import get
+from aiohttp.client import request
+
+
+
+tokens_dict = {
+    'morw' :     '0x6b61b24504a6378e1a99d2aa2a5efcb1f5627a3a',
+    'slp'  :     '0xcc8fa225d80b9c7d42f96e9570156c65d6caaa25',
+    'pvu'  :     '0x31471e0791fcdbe82fbf4c44943255e923f1b794',
+    'eternal' :  '0xd44fd09d74cd13838f137b590497595d6b3feea4'
+}
+
+async def blockchain_connection(ctx, arg1):
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get('https://api.pancakeswap.info/api/v2/tokens/' + tokens_dict[arg1]) as r:
+            res = await r.json()  # returns dict
+            await ctx.reply(res['data']['price'])
+
+
---
+++ BOTS/RPG Discord Game/characters.py
@@ -0,0 +1,7 @@
+# Mague
+
+
+# Assasin
+
+
+# Warrior
