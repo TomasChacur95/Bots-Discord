2155c6258eb6693a352e09d90d2805f6ec4471af
Tom Chacur <tomasdatasc@gmail.com>
2022-03-08 15:18:20 -0300

    CriptoPriceTracker-Changes

--- BOTS/Crypto Price Tracker/blockchain.py
+++ BOTS/Crypto Price Tracker/blockchain.py
@@ -1,5 +1,6 @@
 import discord 
 import json
+import os
 import aiohttp
 from discord.utils  import get
 from aiohttp.client import request
@@ -7,9 +8,8 @@ from aiohttp.client import request
 
 
 tokens_dict = {
-    'morw' :     '0x6b61b24504a6378e1a99d2aa2a5efcb1f5627a3a',
-    'slp'  :     '0xcc8fa225d80b9c7d42f96e9570156c65d6caaa25',
-    'pvu'  :     '0x31471e0791fcdbe82fbf4c44943255e923f1b794',
+    'morw'    :  '0x6b61b24504a6378e1a99d2aa2a5efcb1f5627a3a',
+    'pvu'     :  '0x31471e0791fcdbe82fbf4c44943255e923f1b794',
     'eternal' :  '0xd44fd09d74cd13838f137b590497595d6b3feea4'
 }
 
@@ -17,6 +17,7 @@ async def blockchain_connection(ctx, arg1):
     async with aiohttp.ClientSession() as cs:
         async with cs.get('https://api.pancakeswap.info/api/v2/tokens/' + tokens_dict[arg1]) as r:
             res = await r.json()  # returns dict
-            await ctx.reply(res['data']['price'])
+            await ctx.reply(print(res['data']['price']))
 
-
+            if r not in 'https://api.pancakeswap.info/api/v2/tokens/':
+                ctx.reply("That token is not admited on this bot, please try with another one.")
--- BOTS/Crypto Price Tracker/main.py
+++ BOTS/Crypto Price Tracker/main.py
@@ -1,7 +1,6 @@
+import asyncio
 import discord 
 import json
-import aiohttp
-import random
 from discord        import activity
 from discord.ext    import tasks, commands
 import secrets      as scrt
@@ -13,10 +12,11 @@ from discord.ext.commands.core import check, command
 
 
 
-# bot = scrt.bot
+# Bot
 bot   = commands.Bot(command_prefix = "!")
-
 login = 0
+
+
 
 
 # Login
@@ -26,17 +26,17 @@ async def on_ready():
     print('We have logged in as {0.user}'.format(bot))
     login = 1
     my_background_task.start()
-
+    
 
 
 # Bot Activity with Crypto Prices
 @tasks.loop(seconds=120.0)
 async def my_background_task():
-    await bot.change_presence(activity=discord.Game(name='XLS : '))
-    await bot.change_presence(activity=discord.Game(name='Eternal $ '))
+    await bot.change_presence(activity=discord.Game(name='WoW'))
+    await bot.change_presence(activity=discord.Game(name='Lost Ark'))
 
 
-
+# Import Tokens Dict from blockchain.py
 blq.tokens_dict
 
 
@@ -44,14 +44,13 @@ blq.tokens_dict
 # Coin Price Command
 @bot.command()
 async def coin(ctx, arg1):
-    
-    if blq.tokens_dict == blq.tokens_dict:
+    if blq.tokens_dict == blq.tokens_dict :
         await blq.blockchain_connection (ctx, arg1)
     else:
-        await ctx.reply("The token " + str(arg1) +  " is not in the token list, if you want to add " + str(arg1) + " to the list please use the command : " + '\n' + "!add_token and insert key and value with an space on each")
+        await ctx.reply("The token " + str(arg1) +  " is not in the token list, if you want to add " + str(arg1) + " to the list please use the command : " + '\n' + "!addtoken and insert key and value with an space on each")
         
 
-
+# Add the token that you want
 @bot.command()
 async def add_token(ctx, key, value):
 
@@ -59,6 +58,11 @@ async def add_token(ctx, key, value):
         await ctx.reply('I already have this token on my list, please add another Contract or another Value')
     else:
         blq.tokens_dict[key] = value
+        await ctx.reply('Token added succesfuly')
+ 
+
+
+        
 
  
 bot.run(scrt.bot_token)
